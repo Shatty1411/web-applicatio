76b1c4391172171fed621cda92a9df07049db0b7
1400004 <love.1.alone@hotmail.com>
2015-10-26 19:26:51 +0000

    last

--- helleoSelector.php
+++ helleoSelector.php
@@ -7,7 +7,7 @@
 <body>
 <h1> Where would you like to say hello? </h1><br><hr><br>
 <a href = "http://web53285.azurewebsites.net/helloPrinter.php">Earth</a><br>
-<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Mars</a><br?
+<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Mars</a><br>
 <a href = "http://web53285.azurewebsites.net/helloPrinter.php">Uranus</a>
 </body>
 </html>
