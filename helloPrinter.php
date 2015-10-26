c2c316657bb24efc77687ac5b118888b5ed68fb6
1400004 <love.1.alone@hotmail.com>
2015-10-26 19:24:12 +0000

    last

---
+++ helloPrinter.php
@@ -0,0 +1,21 @@
+<?php
+  $HELLO = $_GET["HELLO"];
+  $EARTH = $_GET["Earth"];
+echo "HELLO" . $EARTH;
+?>
+
+http;//www.mysite.com/helloPrinter.php?planet=Earth
+<?php
+$HELLO = $_GET["HELLO"];
+$Mars= $_GET["Mars"];
+echo "HELLO" . $Mars;
+?>
+
+http;//www.mysite.com/helloPrinter.php?planet=Mars
+<?php
+$HELLO = $_GET["HELLO"];
+$Uranus = $_GET["Uranus"];
+echo "HELLO" . $Uranus;
+?>
+http;//www.mysite.com/helloPrinter.php?planet=Uranus
+
