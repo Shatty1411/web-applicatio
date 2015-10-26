4163e5f5a284896f24a525d3170163248c5a48f4
1400004 <love.1.alone@hotmail.com>
2015-10-26 18:49:46 +0000

    For me

---
+++ phpmyadmin/helleoSelector.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Selector </title>
+</head>
+<body>
+<h1> Where would you like to say hello? </h1><br><hr><br>
+<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Earth</a>
+<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Mars</a>
+<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Uranus</a>
+</body>
+</html>
