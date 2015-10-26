074078135dd27c67734f0e7e1f9a180803800289
1400004 <love.1.alone@hotmail.com>
2015-10-26 19:25:21 +0000

    last

--- helleoSelector.php
+++ helleoSelector.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 <h1> Where would you like to say hello? </h1><br><hr><br>
-<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Earth</a>
-<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Mars</a>
+<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Earth</a><br>
+<a href = "http://web53285.azurewebsites.net/helloPrinter.php">Mars</a><br?
 <a href = "http://web53285.azurewebsites.net/helloPrinter.php">Uranus</a>
 </body>
 </html>
